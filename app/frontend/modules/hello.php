9d4d1efb0481a6b4cd8c4ecd410534a2c0542fa1
Cristian Recabarren <cristian.recabarren.madrid@gmail.com>
2015-07-02 18:12:57 -0300

    Second version, security added to application and module load

---
+++ app/frontend/modules/hello.php
@@ -0,0 +1,7 @@
+<?php
+
+if (!defined('SECURE_APP'))
+    die("you should not be here");
+
+echo $name;
+echo '<a href="/index.php?app=frontend&module=index">Go back Index</a>';
---
+++ app/frontend/modules/index.php
@@ -0,0 +1,7 @@
+<?php
+
+if (!defined('SECURE_APP'))
+    die("you should not be here");
+
+echo "Index<br>";
+echo '<a href="/index.php?app=frontend&module=hello&name=Cristian">Say Hello</a>';
